b9994d82312950373cc9779c8fe1824f05275472
YE GAO <sortsimilar@gmail.com>
2022-08-23 15:41:11 +0800

    Update solver.py

--- Combinatorial optimization/week2/knapsack/solver.py
+++ Combinatorial optimization/week2/knapsack/solver.py
@@ -29,7 +29,7 @@ def solve_it(input_data):
     opt = 0
 
     total_weight = 0
-    taken = [0] * len(items)
+    taken = [0] * len(items)  # a way to create list of certain size;
 
     for item in items:
         if total_weight + item.weight <= capacity:
